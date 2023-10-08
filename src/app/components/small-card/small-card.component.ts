c3c916dfaa2f82341c2d7262cef8d5365e630be0
Alessandra da Costa Vieira <alessandracv.contato@gmail.com>
2023-10-08 09:51:56 -0300

    feat: now all components are dynamic

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string ="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png";
+  @Input()
+  cardTitle:string ="Inedito, Cassie desenvolve uma pokedex!";
 
   constructor() { }
 
