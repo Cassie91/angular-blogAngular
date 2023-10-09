f44043c521b1356be6a1d2cbbc08f61d2e9c5a03
Alessandra da Costa Vieira <alessandracv.contato@gmail.com>
2023-10-09 09:10:50 -0300

    feat: finished first functional blog version.

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string ="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png";
   @Input()
   cardTitle:string ="Inedito, Cassie desenvolve uma pokedex!";
+  @Input()
+  id:string="0";
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":'1',
+    "photo": 'https://static-00.iconduck.com/assets.00/file-type-angular-icon-1907x2048-tobdkjt1.png',
+    "title": 'Cassie finaliza a primeira versão funcional de seu blog com Angular!',
+    "description": 'Cassie conclui com sucesso a primeira versão funcional de seu blog, utilizando a estrutura Angular. Sua abordagem habilidosa e dedicação destacam a eficiência dessa tecnologia no desenvolvimento web.'
+  },
+  {
+    "id":'2',
+    "photo": 'https://cdn.icon-icons.com/icons2/851/PNG/512/Pokedex_tool_icon-icons.com_67529.png',
+    "title": 'Cassie desenvolve uma pokedex utilizando pokeAPI!',
+    "description": 'Cassie está mergulhando no desenvolvimento de uma Pokédex interativa, utilizando suas habilidades em JavaScript, HTML e CSS para extrair dados em tempo real da PokeAPI e proporcionar uma experiência envolvente aos usuários.'
+  },
+  {
+    "id":'3',
+    "photo": 'https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg',
+    "title": 'Content 3 title',
+    "description": 'Content 3 description'
+  },
+  {
+    "id":'4',
+    "photo": 'https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg',
+    "title": 'Content 4 title',
+    "description": 'Content 4 description'
+  }
+]
